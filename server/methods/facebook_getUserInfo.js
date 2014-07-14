70fe4de1185a66d354e6d2adc60cd217fb10304b
Vincil Bishop <vincil.bishop@vbishop.com>
2014-07-14 16:26:51 -0500

    Updated get user info

--- server/methods/facebook_getUserInfo.js
+++ server/methods/facebook_getUserInfo.js
@@ -11,33 +11,6 @@
 Meteor.methods ({
   '/facebook/getUserInfo': function () {
 
-    var FB = new Facebook({
-      appID: Meteor.settings.Facebook.appId,
-      secret: Meteor.settings.Facebook.secret
-    });
-
-    var user = Meteor.users.findOne(this.userId);
-
-    FB.setAccessToken(user.services.facebook.accessToken);
-
-    FB.api('/me', function(err, data) {
-      /*
-       {
-       "id":"10152179306018107",
-       "email":"vincil.bishop@vbishop.com",
-       "first_name":"Vincil",
-       "gender":"male",
-       "last_name":"Bishop",
-       "link":"https://www.facebook.com/app_scoped_user_id/10152179306018107/",
-       "locale":"en_US",
-       "name":"Vincil Bishop",
-       "timezone":-5,
-       "updated_time":"2014-01-11T23:41:29+0000",
-       "verified":true
-       }
-       */
-      console.log('/me: ' + JSON.stringify(data));
-    });
-
+    return Meteor.call('/facebook/currentUser/api','/me');
   }
 });
